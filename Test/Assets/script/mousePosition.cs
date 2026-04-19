34c1c4a8bcc2c7d7dbaa62e0e947859b065bf723
hasib6466 <hasibul.cc24@gmail.com>
2026-04-19 17:42:00 +0600

    Created blank  unity project

---
+++ Test/Assets/script/mousePosition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MousePosition2D : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition; 
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.z = 0f;
+
+        transform.position = Vector2.Lerp(
+            transform.position,
+            mousePosition,
+            moveSpeed * Time.deltaTime
+        );
+    }
+}
